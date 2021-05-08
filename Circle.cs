f645d87d30f5909f0d7cddc85fd0477d073a39e5
Vlad <modproject@ukr.net>
2021-05-08 19:35:15 +0300

    letsgo

---
+++ Circle.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+using SFML.Graphics;
+using SFML.System;
+using System;
+using System.Numerics;
+
+namespace ConsoleApp1
+{
+    struct Circle
+    {
+         CircleShape c;
+
+        public Circle(Color color, float radius)
+        {
+            c = new CircleShape();
+            c.Radius = radius;
+            c.FillColor = color;
+        }
+        
+        public CircleShape ball()
+        {
+            return c;
+        }
+        public void setPos(Vector2f vector)
+        {
+            c.Position = vector;
+        }
+    }
+
+}
---
+++ Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace ConsoleApp1
+{
+    public class Game
+    {
+        public void LetsGo()
+        {
+            RenderWindow window = new(mode: new VideoMode(width: 1280, height: 720), title: "Aero-hockey");
+            window.Closed += WindowClosed;
+
+            Circle circle = new Circle(color: Color.Red, radius: 20);
+            circle.setPos(vector: new Vector2f(x: (window.Size.X / 2)-20, y: (window.Size.Y / 2)-20));                                 
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear(color: Color.White);
+                window.Draw(circle.ball());
+                window.Display();
+            }
+        }
+
+        private void WindowClosed(object sender, EventArgs e)
+        {
+            var w = (RenderWindow) sender;
+            w.Close();
+        }
+    }
+}
